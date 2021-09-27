8d93769fc6ccd2b2608dc04217947b27234eec06
Tadek <tadek.waluga@gmail.com>
2021-09-27 22:26:56 +0200

    Adding file writings24.py for exercise 2 with homework 24

--- figures.py
+++ figures.py
@@ -17,13 +17,54 @@ def greatest_common_divisor(filepath):
             numbers = [int(i) for i in numbers]
             numbers.sort()
             a, b, c = numbers[2], numbers[1], numbers[0]
-            while a % b != 0:
-                gcd = a % b
-                a = b
-                b = gcd
+            if a % b == 0:
+                gcd = b
+            else:
+                while a % b != 0:
+                    gcd = a % b
+                    a = b
+                    b = gcd
+            if c % gcd != 0:
+                d = gcd
+                while c % d != 0:
+                    gcd = c % d
+                    c = d
+                    d = gcd
+            counter += gcd
+        return print(f'Sum of the greatest common divisors is: {counter}')
 
+
+def sum_of_digits_in_the_row(filepath):
+    with open(filepath) as f:
+        counter_sum_35 = 0
+        counter_sum_max = 0
+        sum_digits_max = 0
+        for line in f:
+            digits = line.strip().replace(' ', '')
+            sum_digits = 0
+            for digit in range(len(digits)):
+                sum_digits += int(digits[digit])
+            if sum_digits == 35:
+                counter_sum_35 += 1
+            if sum_digits > sum_digits_max:
+                sum_digits_max = sum_digits
+    with open(filepath) as f:
+        for line in f:
+            digits = line.strip().replace(' ', '')
+            sum_digits = 0
+            for digit in range(len(digits)):
+                sum_digits += int(digits[digit])
+            if sum_digits == sum_digits_max:
+                counter_sum_max += 1
+        return print(f'Number of lines for which the sum of the digits of all three written numbers is equal 35: '
+                     f'{counter_sum_35}\nThe greatest sum of digits in the row: {sum_digits_max} the number of rows '
+                     f'where the sum of the digits is equal to the highest value: {counter_sum_max}')
 
 
 if __name__ == '__main__':
     file_path = 'liczby.txt'
+    # file_path2 = 'liczby2.txt'
+    # file_path3 = 'liczby3.txt'
     numbers_in_ascending_order(file_path)
+    greatest_common_divisor(file_path)
+    sum_of_digits_in_the_row(file_path)
---
+++ writings24.py
@@ -0,0 +1,53 @@
+from math import sqrt
+
+def prime_numbers(filepath):
+    with open(filepath) as f:
+        amount_prime_numbers = 0
+        for line in f:
+            counter = 0
+            factor = 2
+            check = True
+            for char in line.strip():
+                counter += ord(char)
+            while factor <= sqrt(counter) and check:
+                if counter % factor == 0:
+                    check = False
+                factor += 1
+            if check:
+                amount_prime_numbers += 1
+        return print(f'The number of primes is {amount_prime_numbers}')
+
+
+def rising_inscriptions(filepath):
+    with open(filepath) as f:
+        for line in f:
+            check = True
+            line = line.strip()
+            for i in range(len(line)-1):
+                if ord(line[i+1]) < ord(line[i]):
+                    check = False
+            if check:
+                print(line)
+
+
+def repeated_inscriptions(filepath):
+    with open(filepath) as f:
+        writings = []
+        for line in f:
+            line = line.strip()
+            if line not in writings:
+                writings.append(line)
+    with open(filepath) as f:
+        printed_writings = []
+        for line in f:
+            line = line.strip()
+            if line in writings and line not in printed_writings:
+                printed_writings.append(line)
+        return print(printed_writings)
+
+
+if __name__ == '__main__':
+    file_path = 'napis.txt'
+    # prime_numbers(file_path)
+    # rising_inscriptions(file_path)
+    repeated_inscriptions(file_path)
