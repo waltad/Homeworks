bc94b482eb30d232528c413936e34bc87597b51d
GI10 <tadus@op.pl>
2021-09-28 14:57:26 +0200

    Fixed function repeated_inscriptions in file writings24.py

--- writings24.py
+++ writings24.py
@@ -1,5 +1,6 @@
 from math import sqrt
 
+
 def prime_numbers(filepath):
     with open(filepath) as f:
         amount_prime_numbers = 0
@@ -33,17 +34,15 @@ def rising_inscriptions(filepath):
 def repeated_inscriptions(filepath):
     with open(filepath) as f:
         writings = []
+        repeated_writings = []
         for line in f:
             line = line.strip()
             if line not in writings:
                 writings.append(line)
-    with open(filepath) as f:
-        printed_writings = []
-        for line in f:
-            line = line.strip()
-            if line in writings and line not in printed_writings:
-                printed_writings.append(line)
-        return print(printed_writings)
+            else:
+                if line not in repeated_writings:
+                    repeated_writings.append(line)
+        return print(repeated_writings)
 
 
 if __name__ == '__main__':
